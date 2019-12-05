654c26794328f3a9870a3e59cad75a6ab80648ba
Pierre Spring <pierre.spring@caillou.ch>
2019-12-05 11:23:32 +0100

    Revert "first actually working prototype"
    This reverts commit 8a1a735b88de5c34ccb27c630ad56d422739fe74.

--- hammerspoon/super.lua
+++ hammerspoon/super.lua
@@ -1,79 +1,50 @@
--- -- A global variable for the Hyper Mode
--- hyper = hs.hotkey.modal.new({}, 'F17')
-
--- -- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
--- function enterHyperMode()
---   hyper.triggered = false
---   hyper:enter()
--- end
-
--- -- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
--- -- send ESCAPE if no other keys are pressed.
--- function exitHyperMode()
---   hyper:exit()
---   hs.eventtap.keyStroke({}, 'space', 0)
--- end
-
--- -- Bind the Hyper key
--- f18 = hs.hotkey.bind({}, 'F18', enterHyperMode, exitHyperMode)
-
--- hyper:bind({}, 'j', function()
---   hs.eventtap.keyStroke({}, 'left', 0)
--- end)
-
 local eventtap = hs.eventtap
 local eventTypes = hs.eventtap.event.types
 
-local keyMap = {
-  i = 'up', -- 34
-  j = 'left', -- 38
-  k = 'down', -- 40
-  l = 'right', -- 37
-}
-
 local state = {}
 state.isActive = false
+state.outputNextSpace = false
 
 superDuperModeActivationListener = eventtap.new({ eventTypes.keyDown }, function(event)
+  local character = event:getCharacters()
+  hs.logger.new('hyper'):e('down:', character)
 
-  local kc = event:getKeyCode()
-  -- hs.logger.new('hyper'):e(kc)
-
-  if kc == 79 then -- f18 key
-    state.isActive = true;
-    return true
-  end
-
-  if not state.isActive then
-    return
-  end
-
-  local mappedKey = keyMap[hs.keycodes.map[kc]]
-
-  if mappedKey then
-    local modifiers = {}
-    -- Apply the standard modifier keys that are active (if any)
-    for k, v in pairs(event:getFlags()) do
-      table.insert(modifiers, k)
+  if character == ' ' then
+    if not state.outputNextSpace then
+      hs.logger.new('hyper'):e('down-enter', character)
+      -- prevent the printing of the space
+      state.isActive = true
+      state.wasUsed = false
+      return true
+    else
+      state.outputNextSpace = false
     end
-    hs.eventtap.keyStroke(modifiers, mappedKey, 0)
-    return true
   end
 
 end):start()
 
 superDuperModeDeactivationListener = eventtap.new({ eventTypes.keyUp }, function(event)
 
+  local character = event:getCharacters()
+  hs.logger.new('hyper'):e('up:', character)
   if not state.isActive then
     return
   end
+  if character == ' ' then
 
-  local kc = event:getKeyCode()
+    state.isActive = false
 
-  if kc == 79 then -- f18 key
-    state.isActive = false;
-    return true
+    if not state.wasUsed then
+      state.outputNextSpace = true
+      hs.timer.doAfter(0, function ()
+        hs.eventtap.keyStroke({}, 'space', 0)
+      end)
+    end
+
+    return
   end
+
+  state.wasUsed = true
 
 end):start()
 
